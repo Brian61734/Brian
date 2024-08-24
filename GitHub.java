a1d5625d27307689de5a52a461b5f2cbfb82ed1b
brian61734 <br5584645@gmail.com>
2024-08-23 21:33:02 -0300

    hola

---
+++ GitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author PUESTO 1
+ */
+public class GitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
